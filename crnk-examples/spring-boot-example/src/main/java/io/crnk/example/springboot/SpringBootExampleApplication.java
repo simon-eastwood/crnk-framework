a931e6e7ccd97399c1fe56571973e003444ac21f
simon-eastwood <simon-eastwood@users.noreply.github.com>
2017-11-20 12:30:30 +0100

    Update SpringBootExampleApplication.java

--- crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/SpringBootExampleApplication.java
+++ crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/SpringBootExampleApplication.java
@@ -14,6 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+// just for filter
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+
+
 @Configuration
 @RestController
 @SpringBootApplication
@@ -40,4 +53,35 @@ public class SpringBootExampleApplication {
 	}
 }
 
+
+
+
+/**
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MyFilter implements Filter {
+
+    private static final boolean CONDITION = true;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("FILTER INIT");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if(CONDITION==true)
+            chain.doFilter(request,response);
+        else{
+            ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
 
