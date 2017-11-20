b1b389aa027ac3b56c0760d3d90f6b37995b165e
simon-eastwood <simon-eastwood@users.noreply.github.com>
2017-11-20 12:11:18 +0100

    Create Filter.java

---
+++ crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/Filter.java
@@ -0,0 +1,36 @@
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SecurityFilter implements Filter {
+
+    private static final boolean CONDITION = true;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("FILTER INIT");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if(CONDITION==true)
+            chain.doFilter(request,response);
+        else{
+            ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
