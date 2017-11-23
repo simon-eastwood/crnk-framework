a446541b50de944ec4090de8c56c336a0419291c
simon-eastwood <simon-eastwood@users.noreply.github.com>
2017-11-23 10:16:39 +0100

    Create DossierBIRepositoryImpl.java

---
+++ crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/repository/DossierBIRepositoryImpl.java
@@ -0,0 +1,59 @@
+/*
+ * Auto generated from a template
+ */
+package io.crnk.example.springboot.domain.repository;
+
+import io.crnk.core.queryspec.QuerySpec;
+import io.crnk.core.repository.ResourceRepositoryBase;
+import io.crnk.example.springboot.domain.model.DossierBI;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+
+
+/**
+ * ResourceRepositoryBase-based example with the base class providing some base functionality.
+ */
+@Component
+public class DossierBIRepositoryImpl extends ResourceRepositoryBase<DossierBI, String> implements DossierBIRepository {
+
+	private static final AtomicLong ID_GENERATOR = new AtomicLong(124);
+
+	private Map<String, DossierBI> myHashMap = new HashMap<>();
+
+	public DossierBIRepositoryImpl() {
+		super(DossierBI.class);
+
+		save(new DossierBI("121"));
+
+	}
+
+	@Override
+	public synchronized void delete(String aNumber) {
+		myHashMap.remove(aNumber);
+	}
+
+	@Override
+	public synchronized <D extends DossierBI> D save(D e) {
+		if (e.getJsonApiId() == null) {
+			Long l = ID_GENERATOR.getAndIncrement();
+			e.setJsonApiId(l.toString());
+		}
+		myHashMap.put(e.getJsonApiId(), e);
+		return e;
+	}
+
+	@Override
+	public synchronized DossierBIList findAll(QuerySpec querySpec) {
+		DossierBIList list = new DossierBIList();
+		list.setMeta(new DossierBIListMeta());
+		list.setLinks(new DossierBIListLinks());
+		querySpec.apply(myHashMap.values(), list);
+		return list;
+	}
+
+}
