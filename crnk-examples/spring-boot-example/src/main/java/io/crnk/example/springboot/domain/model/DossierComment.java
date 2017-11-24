b062448508fefe4429cb95bd5c1d33f0d26156ab
Simon Eastwood <seastwood@epo.org>
2017-11-24 11:54:03 +0100

    readded dossiercommentsresponse

--- crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/model/DossierComment.java
+++ crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/model/DossierComment.java
@@ -9,19 +9,7 @@ import java.util.List;
 import java.util.Date;
 
 
-@JsonApiResource(type = "dossiercomment")
 public class DossierComment {
-	@JsonApiId
-	private String jsonApiId;
-	public String getJsonApiId() {return this.jsonApiId;}
-	public void setJsonApiId(String id) { this.jsonApiId = id;}
-	public DossierComment(String id) {this.jsonApiId = id;}
-
-
-	@JsonProperty
-	private String eTag = null;
-	public String getEtag() {return this.eTag;}
-	public void setEtag(String etag) { this.eTag  = etag ;}
 
 	public DossierComment() {};
     
