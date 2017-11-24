2f2eabb13f04e18f201d2d198948b5b39ed7ea4a
Simon Eastwood <seastwood@epo.org>
2017-11-24 11:20:36 +0100

    added dossier comments

--- crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/model/DossierCommentsResponse.java
+++ crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/model/DossierCommentsResponse.java
@@ -9,7 +9,19 @@ import java.util.List;
 import java.util.Date;
 
 
+@JsonApiResource(type = "dossiercommentsresponse")
 public class DossierCommentsResponse {
+	@JsonApiId
+	private String jsonApiId;
+	public String getJsonApiId() {return this.jsonApiId;}
+	public void setJsonApiId(String id) { this.jsonApiId = id;}
+	public DossierCommentsResponse(String id) {this.jsonApiId = id;}
+
+
+	@JsonProperty
+	private String eTag = null;
+	public String getEtag() {return this.eTag;}
+	public void setEtag(String etag) { this.eTag  = etag ;}
 
 	public DossierCommentsResponse() {};
     
