fdcf91eb3412169c97e350190dba812ea8063155
Simon Eastwood <seastwood@epo.org>
2017-12-19 14:34:52 +0100

    rollback tpresource read to read from jar

--- crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/repository/TunnelRepositoryImpl.java
+++ crnk-examples/spring-boot-example/src/main/java/io/crnk/example/springboot/domain/repository/TunnelRepositoryImpl.java
@@ -3,22 +3,40 @@
  */
 package io.crnk.example.springboot.domain.repository;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.stereotype.Component;
-
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ResourceRepositoryBase;
 import io.crnk.example.springboot.domain.model.Tunnel;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import java.io.*;
+import java.net.URL;
+import java.security.CodeSource;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 /**
  * ResourceRepositoryBase-based example with the base class providing some base functionality.
@@ -32,14 +50,17 @@ public class TunnelRepositoryImpl extends ResourceRepositoryBase<Tunnel, String>
 
 	public TunnelRepositoryImpl() {
 		super(Tunnel.class);
-		System.out.println("This is tunnel");
+
+
 		try {
-			this.getResourceFolderFiles("tpresources");
-			//this.loadFromJar();
+
+			this.loadFromJar();
 		} catch (Exception e) {
-			System.out.println("Cannot load any files");
+			System.out.println("Cannot load any files from jar ");
 			System.out.println(e);
 		}
+		
+
 	}
 
 	@Override
@@ -53,65 +74,61 @@ public class TunnelRepositoryImpl extends ResourceRepositoryBase<Tunnel, String>
 			Long l = ID_GENERATOR.getAndIncrement();
 			e.setJsonApiId(l.toString());
 		}
-		e.seteTag(String.valueOf(System.currentTimeMillis()));
+		e.setEtag(String.valueOf(System.currentTimeMillis()));
 		myHashMap.put(e.getJsonApiId(), e);
 		return e;
 	}
 
 	@Override
 	public synchronized TunnelList findAll(QuerySpec querySpec) {
-		System.out.println("Tunnel findAll gets called");
 		TunnelList list = new TunnelList();
 		list.setMeta(new TunnelListMeta());
 		list.setLinks(new TunnelListLinks());
 		querySpec.apply(myHashMap.values(), list);
 		return list;
 	}
-	
-	private void getResourceFolderFiles (String folder) {
-	    ClassLoader loader = Thread.currentThread().getContextClassLoader();
-	    //read from the resources folder
-	    URL url = loader.getResource(folder);
-	    String path = url.getPath();
-	    File file = new File(path);
-	    displayDirectoryContents(file);
-	}
-	
-	public void displayDirectoryContents(File dir) {
-		try {
-			File[] files = dir.listFiles();
-			for (File file : files) {
-				if (file.isDirectory()) {
-					displayDirectoryContents(file);
-				} else {
-					FileInputStream fis = null;
-					 if (file.getCanonicalPath().endsWith(".json") || file.getCanonicalPath().endsWith(".xml")) {
-	                        System.out.println( "Full Path :"+file.getCanonicalPath() );
-	                        try {
-	        					fis = new FileInputStream(file);
-	        				} catch (FileNotFoundException e) {
-	        					// TODO Auto-generated catch block
-	        					e.printStackTrace();
-	        				}
-	                        BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-	                        String line;
-	                        StringBuilder out = new StringBuilder();
-	                        try {
-	                            while ((line = reader.readLine()) != null) {
-	                                out.append(line);
-	                            }
-	                        } catch (IOException e) {
-	                            // do something, probably not a text file
-	                            e.printStackTrace();
-	                        }
-	        				Tunnel t = new Tunnel (file.getName());
-	        				t.payload = out.toString()  ;
-	        				this.save(t);
-					 }
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
+
+        private void loadFromJar() throws Exception {
+            CodeSource src = TunnelRepositoryImpl.class.getProtectionDomain().getCodeSource();
+            List<String> list = new ArrayList<String>();
+
+            if (src != null) {
+                URL jar = src.getLocation();
+                ZipInputStream zip = new ZipInputStream(jar.openStream());
+                ZipEntry ze = null;
+
+                while ((ze = zip.getNextEntry()) != null) {
+                    String entryName = ze.getName();
+
+                    if (entryName.startsWith("tpresources") && (entryName.endsWith(".json") || entryName.endsWith(".xml")) ) {
+                        System.out.println( entryName );
+                        StringBuilder s = readTxtFile( zip );
+
+			Tunnel t = new Tunnel (entryName);
+			t.payload = s.toString()  ;
+			this.save (t);
+                    }
+                }
+
+            }
+
+        }
+
+        private  StringBuilder readTxtFile(InputStream in)  {
+            StringBuilder out = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    out.append(line);
+                }
+            } catch (IOException e) {
+                // do something, probably not a text file
+                e.printStackTrace();
+            }
+            return out;
+        }
+
+
 }
